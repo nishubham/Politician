f23c7b87b0974eee7cd1a945eff4ccaedb387457
Shubham Kumar Ranjan <nishubham8080@gmail.com>
2026-02-13 17:50:25 +0530

    Entity Class

---
+++ src/main/java/com/shubham/ProjectOnRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.shubham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOnRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOnRestApiApplication.class, args);
+	}
+
+}
