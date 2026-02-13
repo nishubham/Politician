7afb5bf40211bb4432a8bf216a17ce2a229136c2
Rajkumar <rajrajput9165570650@gmail.com>
2026-02-13 19:01:37 +0530

    commit by raj

---
+++ src/main/java/com/shubham/service/PoliticianService.java
@@ -0,0 +1,28 @@
+package com.shubham.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shubham.entity.Politician;
+import com.shubham.repository.PoliticianRepository;
+
+@Service
+public class PoliticianService {
+	
+	@Autowired
+	private PoliticianRepository repo;
+	
+
+	public String add(Politician poli){
+		Politician save = repo.save(poli);
+		return save.getName()+" is save";
+	}
+	
+	
+	public List<Politician> findAll(){
+		return repo.findAll();
+	}
+
+}
